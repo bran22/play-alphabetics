f0cf5eb991a29133b2e2d34c4ce716c44720ba42
bran22 <8986312+bran22@users.noreply.github.com>
2022-09-16 00:53:52 -0400

    implementing logic to generate game words

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { allWords, alphabet, Letter, WordList } from './words';
 
+const getRandomWord = (words: string[]): string => words[Math.floor(Math.random() * words.length)];
+const getGameWords = (startingLetterIndex: number, allWords: WordList): string[] => {
+  return alphabet
+    .slice(startingLetterIndex, startingLetterIndex + 10)
+    .map( letter => allWords[letter] )
+    .map( getRandomWord );
+}
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
@@ -10,6 +18,11 @@ export class PlayComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const startingLetterIndex = Math.round(
+      Math.random() * (alphabet.length - 10)
+    );
+    const gameWords = getGameWords(startingLetterIndex, allWords);
+    console.log(gameWords);
   }
 
 }
