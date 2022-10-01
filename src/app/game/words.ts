a8673cf0f1993fd6be03509c082c5aceeac72c0d
bran22 <8986312+bran22@users.noreply.github.com>
2022-10-01 01:23:56 -0400

    adding some more words

--- src/app/game/words.ts
+++ src/app/game/words.ts
@@ -4,30 +4,125 @@ export const alphabet: Letter[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
   'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 export const allWords: WordList = {
-  a: ['apple', 'avocado', 'aardvark', 'abbey', 'abacus', 'accent', 'accept', 'adorn', 'agency', 'agile', 'almond', 'alumni', 'amaze', 'absurd', 'again', 'ahead', 'alive', 'angel', 'axle', 'award', 'awful', 'admire', 'agenda', 'aggressive', 'airport', 'allegro', 'amulet', 'anxiety', 'apology', 'acute', 'attire'],
-  b: ['banana', 'brave', 'balloon', 'bathe', 'blame', 'broom', 'brain', 'beckon', 'bicep', 'blade', 'blouse', 'boil', 'border', 'bandana', 'battle', 'burp', 'berserk', 'betray', 'bias', 'bleeding', 'boast', 'boats', 'boulder', 'books', 'boycott', 'breathe', 'backfire', 'baklava', 'bedding', 'befriend'],
-  c: ['cat', 'candy', 'cricket', 'cabin', 'cart', 'cedar', 'cheap', 'clamp', 'civic', 'canoe', 'carpet', 'castle', 'cherry', 'cheeks', 'cabbage', 'chewy', 'citrus', 'chord', 'circle', 'critique', 'coil', 'copper', 'commit', 'campfire', 'cantina', 'caramel', 'casual', 'cactus', 'caffeine', 'calcium'],
-  d: ['dove', 'dog', 'domino', 'duck', 'duet', 'dance', 'debut', 'deter', 'dodge', 'dabble', 'dagger', 'debate', 'defer', 'degree', 'detach', 'drought', 'drink', 'dunk', 'daffodil', 'default', 'defeated', 'delete', 'darling', 'daybreak', 'decaying', 'determined', 'describe', 'defend', 'devotion', 'diagonal', 'decline'],
-  e: ['elephant', 'ear', 'earth', 'eel', 'elite', 'enter', 'erode', 'exam', 'eager', 'either', 'empire', 'enable', 'engaged', 'equals', 'error', 'ethical', 'exiled', 'eyelid', 'easy', 'ellipse', 'embargo', 'enamel', 'encode', 'eclipse', 'ensnare', 'episode', 'erase', 'erosion', 'escape', 'exhaust'],
-  f: ['fish', 'fancy', 'finite', 'fry', 'fame', 'feud', 'foil', 'fork', 'fable', 'flaky', 'flood', 'focus', 'faucet', 'fantastic', 'fever', 'filmed', 'first', 'fluent', 'flying', 'flute', 'fajitas', 'falafel', 'fashion', 'fatigue', 'favorite', 'fetch', 'fizzle', 'foreign', 'farming', 'freight', 'faint'],
-  g: ['glove', 'golf', 'gleam', 'glazed', 'glide', 'gaudy', 'green', 'grime', 'gumbo', 'gimmick', 'gyros', 'gallon', 'galaxy', 'gaming', 'garden', 'garlic', 'gaze', 'glitter', 'gifted', 'glass', 'globe', 'goofy', 'grains', 'grapes', 'gummy', 'gadget', 'generic', 'geology', 'glitch', 'glacier', 'glory'],
-  h: ['hippo', 'hotel', 'hearty', 'harp', 'halo', 'hook', 'harbor', 'harmony', 'heard', 'hive', 'hound', 'hatred', 'heated', 'hereby', 'hunger', 'holding', 'hoops', 'hubris', 'halved', 'handbag', 'hangar', 'hardship', 'hasty', 'header', 'hefty', 'helium', 'herself', 'himself', 'honesty', 'horse', 'hyphen'],
-  i: ['iguana', 'igloo', 'impulse', 'iced', 'isle', 'image', 'inbox', 'immune', 'illegal', 'itchy', 'idiot', 'impact', 'injury', 'itself', 'immersion', 'inhabit', 'integer', 'iron', 'itemize', 'isolate', 'ignite', 'illusion', 'intimidate', 'impolite', 'industry', 'infinity', 'instincts', 'intruder', 'ignore'],
-  j: ['jester', 'jaguar', 'junction', 'jazz', 'joke', 'just', 'juice', 'jumbo', 'jigsaw', 'jumped', 'juggle', 'jungle', 'jackpot', 'jammed', 'jersey', 'jogging', 'jolt', 'justice', 'jewelry', 'joint', 'juvenile', 'junk', 'jellyfish', 'jeopardize', 'journalist', 'jargon', 'jolly', 'junior', 'journal', 'judgment'],
-  k: ['king', 'kilogram', 'knife', 'kind', 'keen', 'knees', 'key', 'kite', 'kitchen', 'karaoke', 'keyboard', 'ketchup', 'knock', 'kale', 'kelp', 'kidnap', 'kick', 'knot', 'knowledge', 'kimchi', 'kinetic', 'keratin', 'koala', 'kazoo', 'kraken', 'karate', 'knit', 'kabob', 'kaboom', 'kayak', 'kiosk'],
-  l: ['lion', 'llama', 'laser', 'lamp', 'laws', 'leapt', 'lucky', 'learned', 'lobby', 'lounge', 'lately', 'lawyer', 'least', 'levitate', 'liquid', 'loaf', 'lottery', 'luxurious', 'launch', 'laundry', 'linear', 'location', 'longbow', 'luggage', 'lyrics', 'label', 'leggings', 'lavender', 'leafy', 'legendary', 'liberate', 'lobster'],
-  m: ['monster', 'medicine', 'melt', 'march', 'mark', 'mean', 'major', 'mild', 'minty', 'money', 'moving', 'motivate', 'myth', 'magical', 'mango', 'mayhem', 'merge', 'method', 'middle', 'mildly', 'mostly', 'mock', 'machine', 'manatee', 'mammoth', 'map', 'marsh', 'mayor', 'menace', 'merchandise', 'microscope'],
-  n: ['noun', 'nice', 'novice', 'near', 'nest', 'nerdy', 'nut', 'nation', 'nasal', 'nearby', 'nickel', 'nimble', 'nobody', 'notice', 'notion', 'numbers', 'nature', 'nectar', 'necklace', 'negate', 'noble', 'noisy', 'noodle', 'novelty', 'navigate', 'neutral', 'nominate', 'nonsense', 'nuclear', 'narrate'],
-  o: ['octopus', 'olive', 'oatmeal', 'orb', 'ocean', 'odor', 'optical', 'ought', 'owner', 'obtuse', 'oldest', 'onward', 'oracle', 'outage', 'otter', 'oxygen', 'obscure', 'offense', 'ominous', 'opaque', 'orchid', 'osmosis', 'outgrow', 'outlaw', 'outstanding', 'oversee', 'obstacle', 'orient'],
-  p: ['pencil', 'penguin', 'palindrome', 'pen', 'pun', 'pail', 'pelt', 'paint', 'panic', 'perch', 'pinch', 'pillar', 'plaza', 'proud', 'promotion', 'panel', 'paddle', 'papers', 'parking', 'parsley', 'pauper', 'peanut', 'peril', 'phlegm', 'pickled', 'pixel', 'piston', 'peace', 'pollen', 'proven'],
-  q: ['queen', 'quilt', 'quiver', 'quiz', 'quart', 'quack', 'quads', 'qualm', 'quake', 'quell', 'quill', 'query', 'quirk', 'quote', 'quota', 'quiet', 'quitting', 'quail', 'quarry', 'quadratic', 'qualify', 'quality', 'quicken', 'quickly', 'quantify', 'quest', 'quotient', 'quadruple'],
-  r: ['rooster', 'rat', 'realm', 'rain', 'real', 'rental', 'rebel', 'reins', 'reuse', 'rhyme', 'resemble', 'rocks', 'rotate', 'relevant', 'racing', 'radish', 'radiation', 'raid', 'random', 'rattle', 'rebate', 'recipe', 'reheat', 'rectify', 'repair', 'recommend', 'resize', 'rinse', 'rookie', 'ruined'],
-  s: ['snake', 'savannah', 'spork', 'spare', 'saucy', 'serious', 'saga', 'saute', 'sense', 'shaky', 'salty', 'shore', 'slang', 'snarl', 'sonic', 'safari', 'salmon', 'saying', 'scorch', 'scald', 'seasoned', 'shaped', 'shoes', 'silent', 'slimy', 'smiling', 'sailing', 'solemn', 'sandbox', 'scarlet'],
-  t: ['tulip', 'tunic', 'tasty', 'tail', 'tear', 'tasks', 'taunt', 'thank', 'tiger', 'topic', 'traded', 'talking', 'tenant', 'tense', 'things', 'thorny', 'thrifty', 'toasty', 'tonic', 'tower', 'training', 'tapioca', 'tatters', 'teapot', 'tempting', 'termite', 'terrain', 'thermal', 'tissue'],
-  u: ['umpire', 'uniform', 'utility', 'ugly', 'undo', 'used', 'unity', 'unbox', 'unite', 'urban', 'usual', 'uncurl', 'uneasy', 'unicorn', 'unlike', 'unlock', 'uproot', 'upsell', 'upset', 'uprising', 'ukulele', 'unplugged', 'unruly', 'upkeep', 'upwards', 'useful', 'utopia', 'utensil'],
-  v: ['velvet', 'velociraptor', 'vermin', 'van', 'vest', 'video', 'vague', 'vents', 'vines', 'voucher', 'vapor', 'venom', 'vexing', 'violin', 'virtue', 'volume', 'voyage', 'vaguely', 'vantage', 'variety', 'vulture', 'venison', 'verdict', 'victim', 'viking', 'visitor', 'vengeance', 'verbatim'],
-  w: ['water', 'whale', 'wilt', 'walk', 'wand', 'whine', 'waltz', 'wept', 'width', 'window', 'witch', 'wooden', 'wring', 'walnut', 'willow', 'weaken', 'wedge', 'wheels', 'wires', 'wished', 'wounded', 'wrist', 'wasted', 'whoever', 'winning', 'washed', 'wrinkly', 'written', 'wreath'],
-  x: ['xenon', 'xylophone', 'xylem', 'xerox', 'xylitol', 'xenophobia'],
-  y: ['yellow', 'yarn', 'yacht', 'yum', 'yelp', 'yawn', 'yoga', 'yolk', 'youth', 'yard', 'yogurt', 'yodel', 'younger', 'yucky', 'yuletide', 'yardstick', 'yesterday', 'yahoo', 'yeast', 'yelling', 'yours', 'yank'],
-  z: ['zany', 'zebra', 'zipper', 'zits', 'zesty', 'zinc', 'zone', 'zoo', 'zenith', 'zodiac', 'zombie', 'zealous', 'zoology', 'zoom', 'zeppelin', 'zucchini', 'zapped', 'zillion'],
+  a: [
+    'apple', 'avocado', 'aardvark', 'abbey', 'abacus', 'accent', 'accept', 'adorn', 'agency', 'agile', 'almond', 'alumni', 'amaze', 'absurd', 'again', 'ahead',
+    'alive', 'angel', 'axle', 'award', 'awful', 'admire', 'agenda', 'aggressive', 'airport', 'allegro', 'amulet', 'anxiety', 'apology', 'acute', 'attire', 'air',
+    'above', 'abyss', 'abort', 'acorn', 'actor', 'adjust', 'aging', 'alpha', 'amused', 'antsy', 'armor', 'arrow', 'await', 'awake', 'avoid', 'altar', 'analog'
+  ],
+  b: [
+    'banana', 'brave', 'balloon', 'bathe', 'blame', 'broom', 'brain', 'beckon', 'bicep', 'blade', 'blouse', 'boil', 'border', 'bandana', 'battle', 'burp', 'berserk',
+    'betray', 'bias', 'bleeding', 'boast', 'boats', 'boulder', 'books', 'boycott', 'breathe', 'backfire', 'baklava', 'bedding', 'befriend', 'bawl', 'bean', 'brow',
+    'burn', 'batch', 'being', 'beginning', 'bites', 'boing', 'bonus', 'brink', 'bully', 'bulky', 'barter', 'barber', 'baton', 'birdie', 'blonde', 'boiled', 'bouncy'
+  ],
+  c: [
+    'cat', 'candy', 'cricket', 'cabin', 'cart', 'cedar', 'cheap', 'clamp', 'civic', 'canoe', 'carpet', 'castle', 'cherry', 'cheeks', 'cabbage', 'chewy', 'citrus',
+    'chord', 'circle', 'critique', 'coil', 'copper', 'commit', 'campfire', 'cantina', 'caramel', 'casual', 'cactus', 'caffeine', 'calcium', 'cape', 'cell', 'claw',
+    'cook', 'core', 'corny', 'crops', 'cuff', 'cure', 'chocolate', 'crane', 'camping', 'carry', 'cent', 'chimp', 'chill', 'chute', 'clams', 'clues', 'comic', 'comma',
+    'comet', 'covered', 'cracked', 'centimeter', 'catchy', 'charge', 'cheetah', 'chisel', 'choice', 'chives', 'chosen', 'churn', 'clasp', 'clock', 'cloudy', 'cookie'
+  ],
+  d: [
+    'dove', 'dog', 'domino', 'duck', 'duet', 'dance', 'debut', 'deter', 'dodge', 'dabble', 'dagger', 'debate', 'defer', 'degree', 'detach', 'drought', 'drink',
+    'dunk', 'daffodil', 'default', 'defeated', 'delete', 'darling', 'daybreak', 'decaying', 'determined', 'describe', 'defend', 'devotion', 'diagonal', 'decline',
+    'disc', 'doors', 'dragged', 'dusty', 'dyed', 'demon', 'diver', 'doing', 'doom', 'drawing', 'drama', 'dress', 'drone', 'dwarf', 'dampen', 'deface', 'defog',
+    'define', 'delta', 'depict', 'deploy', 'detour', 'dinner', 'direction', 'director', 'donation', 'depart', 'dentist', 'defect', 'device', 'diesel', 'dining',
+    'dishes', 'divine', 'diving', 'dosage', 'drier', 'dried', 'disability', 'decorative', 'defective', 'definitely', 'discipline'
+  ],
+  e: [
+    'elephant', 'ear', 'earth', 'eel', 'elite', 'enter', 'erode', 'exam', 'eager', 'either', 'empire', 'enable', 'engaged', 'equals', 'error', 'ethical', 'exiled',
+    'eyelid', 'easy', 'ellipse', 'embargo', 'enamel', 'encode', 'eclipse', 'ensnare', 'episode', 'erase', 'erosion', 'escape', 'exhaust', 'envy', 'even',
+    'ever', 'etch', 'examine', 'easel', 'eerie', 'eight', 'elbow', 'elegy', 'elf', 'eczema', 'echo', 'edible', 'early', 'eclair', 'earrings', 'earmuff', 'earphones', 
+    'eave', 'eject', 'ebony', 'evil', 'epic', 'egg', 'era', 'eon', 'existential', 'eagle', 'eraser', 'edge', 'elk', 'editor', 'easting', 'eavesdrop', 'eclipse', 
+    'eliptical', 'earthquake', 'ego', 'effective', 'effervescent', 'electricity', 'electron', 'education', 'embelishment', 'engineer', 'ember', 'emoji', 'enter', 
+    'evict', 'eight', 'elixir', 'enigma', 'engine', 'excel', 'expire', 'edamame'
+  ],
+  f: [
+    'fish', 'fancy', 'finite', 'fry', 'fame', 'feud', 'foil', 'fork', 'fable', 'flaky', 'flood', 'focus', 'faucet', 'fantastic', 'fever', 'filmed', 'first',
+    'fluent', 'flying', 'flute', 'fajitas', 'falafel', 'fashion', 'fatigue', 'favorite', 'fetch', 'fizzle', 'foreign', 'farming', 'freight', 'faint', 'fairy',
+    'fatty', 'feast', 'feud', 'filet', 'finch', 'fists', 'fleet', 'flesh', 'font', 'fryer', 'fungus', 'fatten', 'flatter', 'fellow', 'fell', 'fibers', 'fickle',
+    'finish', 'filthy', 'fritters', 'fix', 'fixture', 'flail', 'flashy', 'flash', 'fire', 'fiery', 'floss', 'floral', 'fluff', 'fluffy', 'flunk', 'folder',
+    'fold', 'fooled', 'fool', 'force', 'forced', 'forage', 'forest', 'frown', 'frothy', 'frosty', 'fury', 'fur', 'furious', 'fascinating', 'flamingo', 'flutter'
+  ],
+  g: [
+    'glove', 'golf', 'gleam', 'glazed', 'glide', 'gaudy', 'green', 'grime', 'gumbo', 'gimmick', 'gyros', 'gallon', 'galaxy', 'gaming', 'garden', 'garlic', 'gaze',
+    'glitter', 'gifted', 'glass', 'globe', 'goofy', 'grains', 'grapes', 'gummy', 'gadget', 'generic', 'geology', 'glitch', 'glacier', 'glory', 'gate', 'goose',
+    'genie', 'ghost', 'ghoul', 'giant', 'gift', 'girly', 'gland', 'glare', 'glee', 'glum', 'gnaw', 'godly', 'gram', 'grammar', 'graph', 'grinning', 'grin', 'guide',
+    'guts', 'gusto', 'gamble', 'guard', 'guardian', 'garland', 'garnish', 'gazebo', 'gentle', 'gently', 'genre', 'giggle', 'glance', 'glad', 'goblin', 'golfer',
+    'grand', 'grandma', 'grandpa', 'grant', 'greed', 'guarantee', 'grease', 'group', 'gravel', 'guilt', 'guilty', 'greenhouse', 'gathering', 'generator', 'graffiti'
+  ],
+  h: [
+    'hippo', 'hotel', 'hearty', 'harp', 'halo', 'hook', 'harbor', 'harmony', 'heard', 'hive', 'hound', 'hatred', 'heated', 'hereby', 'hunger', 'holding', 'hoops',
+    'hubris', 'halved', 'handbag', 'hangar', 'hardship', 'hasty', 'header', 'hefty', 'helium', 'herself', 'himself', 'honesty', 'horse', 'hyphen'
+  ],
+  i: [
+    'iguana', 'igloo', 'impulse', 'iced', 'isle', 'image', 'inbox', 'immune', 'illegal', 'itchy', 'idiot', 'impact', 'injury', 'itself', 'immersion', 'inhabit',
+    'integer', 'iron', 'itemize', 'isolate', 'ignite', 'illusion', 'intimidate', 'impolite', 'industry', 'infinity', 'instincts', 'intruder', 'ignore'
+  ],
+  j: [
+    'jester', 'jaguar', 'junction', 'jazz', 'joke', 'just', 'juice', 'jumbo', 'jigsaw', 'jumped', 'juggle', 'jungle', 'jackpot', 'jammed', 'jersey', 'jogging',
+    'jolt', 'justice', 'jewelry', 'joint', 'juvenile', 'junk', 'jellyfish', 'jeopardize', 'journalist', 'jargon', 'jolly', 'junior', 'journal', 'judgment'
+  ],
+  k: [
+    'king', 'kilogram', 'knife', 'kind', 'keen', 'knees', 'key', 'kite', 'kitchen', 'karaoke', 'keyboard', 'ketchup', 'knock', 'kale', 'kelp', 'kidnap', 'kick',
+    'knot', 'knowledge', 'kimchi', 'kinetic', 'keratin', 'koala', 'kazoo', 'kraken', 'karate', 'knit', 'kabob', 'kaboom', 'kayak', 'kiosk'
+  ],
+  l: [
+    'lion', 'llama', 'laser', 'lamp', 'laws', 'leapt', 'lucky', 'learned', 'lobby', 'lounge', 'lately', 'lawyer', 'least', 'levitate', 'liquid', 'loaf', 'lottery',
+    'luxurious', 'launch', 'laundry', 'linear', 'location', 'longbow', 'luggage', 'lyrics', 'label', 'leggings', 'lavender', 'leafy', 'legendary', 'liberate',
+    'lobster'
+  ],
+  m: [
+    'monster', 'medicine', 'melt', 'march', 'mark', 'mean', 'major', 'mild', 'minty', 'money', 'moving', 'motivate', 'myth', 'magical', 'mango', 'mayhem', 'merge',
+    'method', 'middle', 'mildly', 'mostly', 'mock', 'machine', 'manatee', 'mammoth', 'map', 'marsh', 'mayor', 'menace', 'merchandise', 'microscope'
+  ],
+  n: [
+    'noun', 'nice', 'novice', 'near', 'nest', 'nerdy', 'nut', 'nation', 'nasal', 'nearby', 'nickel', 'nimble', 'nobody', 'notice', 'notion', 'numbers', 'nature',
+    'nectar', 'necklace', 'negate', 'noble', 'noisy', 'noodle', 'novelty', 'navigate', 'neutral', 'nominate', 'nonsense', 'nuclear', 'narrate'
+  ],
+  o: [
+    'octopus', 'olive', 'oatmeal', 'orb', 'ocean', 'odor', 'optical', 'ought', 'owner', 'obtuse', 'oldest', 'onward', 'oracle', 'outage', 'otter', 'oxygen',
+    'obscure', 'offense', 'ominous', 'opaque', 'orchid', 'osmosis', 'outgrow', 'outlaw', 'outstanding', 'oversee', 'obstacle', 'orient'
+  ],
+  p: [
+    'pencil', 'penguin', 'palindrome', 'pen', 'pun', 'pail', 'pelt', 'paint', 'panic', 'perch', 'pinch', 'pillar', 'plaza', 'proud', 'promotion', 'panel', 'paddle',
+    'papers', 'parking', 'parsley', 'pauper', 'peanut', 'peril', 'phlegm', 'pickled', 'pixel', 'piston', 'peace', 'pollen', 'proven'
+  ],
+  q: [
+    'queen', 'quilt', 'quiver', 'quiz', 'quart', 'quack', 'quads', 'qualm', 'quake', 'quell', 'quill', 'query', 'quirk', 'quote', 'quota', 'quiet', 'quitting',
+    'quail', 'quarry', 'quadratic', 'qualify', 'quality', 'quicken', 'quickly', 'quantify', 'quest', 'quotient', 'quadruple'
+  ],
+  r: [
+    'rooster', 'rat', 'realm', 'rain', 'real', 'rental', 'rebel', 'reins', 'reuse', 'rhyme', 'resemble', 'rocks', 'rotate', 'relevant', 'racing', 'radish',
+    'radiation', 'raid', 'random', 'rattle', 'rebate', 'recipe', 'reheat', 'rectify', 'repair', 'recommend', 'resize', 'rinse', 'rookie', 'ruined'
+  ],
+  s: [
+    'snake', 'savannah', 'spork', 'spare', 'saucy', 'serious', 'saga', 'saute', 'sense', 'shaky', 'salty', 'shore', 'slang', 'snarl', 'sonic', 'safari', 'salmon',
+    'saying', 'scorch', 'scald', 'seasoned', 'shaped', 'shoes', 'silent', 'slimy', 'smiling', 'sailing', 'solemn', 'sandbox', 'scarlet'
+  ],
+  t: [
+    'tulip', 'tunic', 'tasty', 'tail', 'tear', 'tasks', 'taunt', 'thank', 'tiger', 'topic', 'traded', 'talking', 'tenant', 'tense', 'things', 'thorny', 'thrifty',
+    'toasty', 'tonic', 'tower', 'training', 'tapioca', 'tatters', 'teapot', 'tempting', 'termite', 'terrain', 'thermal', 'tissue'
+  ],
+  u: [
+    'umpire', 'uniform', 'utility', 'ugly', 'undo', 'used', 'unity', 'unbox', 'unite', 'urban', 'usual', 'uncurl', 'uneasy', 'unicorn', 'unlike', 'unlock', 'uproot',
+    'upsell', 'upset', 'uprising', 'ukulele', 'unplugged', 'unruly', 'upkeep', 'upwards', 'useful', 'utopia', 'utensil'
+  ],
+  v: [
+    'velvet', 'velociraptor', 'vermin', 'van', 'vest', 'video', 'vague', 'vents', 'vines', 'voucher', 'vapor', 'venom', 'vexing', 'violin', 'virtue', 'volume',
+    'voyage', 'vaguely', 'vantage', 'variety', 'vulture', 'venison', 'verdict', 'victim', 'viking', 'visitor', 'vengeance', 'verbatim'
+  ],
+  w: [
+    'water', 'whale', 'wilt', 'walk', 'wand', 'whine', 'waltz', 'wept', 'width', 'window', 'witch', 'wooden', 'wring', 'walnut', 'willow', 'weaken', 'wedge', 'wheels',
+    'wires', 'wished', 'wounded', 'wrist', 'wasted', 'whoever', 'winning', 'washed', 'wrinkly', 'written', 'wreath'
+  ],
+  x: [
+    'xenon', 'xylophone', 'xylem', 'xerox', 'xylitol', 'xenophobia'
+  ],
+  y: [
+    'yellow', 'yarn', 'yacht', 'yum', 'yelp', 'yawn', 'yoga', 'yolk', 'youth', 'yard', 'yogurt', 'yodel', 'younger', 'yucky', 'yuletide', 'yardstick', 'yesterday',
+    'yahoo', 'yeast', 'yelling', 'yours', 'yank'
+  ],
+  z: [
+    'zany', 'zebra', 'zipper', 'zits', 'zesty', 'zinc', 'zone', 'zoo', 'zenith', 'zodiac', 'zombie', 'zealous', 'zoology', 'zoom', 'zeppelin', 'zucchini', 'zapped',
+    'zillion'
+  ],
 };
