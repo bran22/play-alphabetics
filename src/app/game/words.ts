93e78d8f95c514c06c1af861587178d1d8694028
bran22 <8986312+bran22@users.noreply.github.com>
2022-10-02 01:37:08 -0400

    adding words

--- src/app/game/words.ts
+++ src/app/game/words.ts
@@ -76,37 +76,82 @@ export const allWords: WordList = {
     'keg', 'kerosene', 'kindness', 'knockout', 'knockoff', 'kaleidoscope'
   ],
   l: [
-    'lion', 'llama', 'laser', 'lamp', 'laws', 'leapt', 'lucky', 'learned', 'lobby', 'lounge', 'lately', 'lawyer', 'least', 'levitate', 'liquid', 'loaf', 'lottery',
+    'lion', 'llama', 'laser', 'lamp', 'law', 'leapt', 'lucky', 'learned', 'lobby', 'lounge', 'lately', 'lawyer', 'least', 'levitate', 'liquid', 'loaf', 'lottery',
     'luxurious', 'launch', 'laundry', 'linear', 'location', 'longbow', 'luggage', 'lyrics', 'label', 'leggings', 'lavender', 'leafy', 'legendary', 'liberate',
-    'lobster', 'lair', 'lint', 'lip', 'load', 'loan', 'loud', 'lane', 'lapel', 'laugh', 'lava', 'light', 'local', 'loopy', 'lousy', 'lure', 'late', 'league', 'latch'
+    'lobster', 'lair', 'lint', 'lip', 'load', 'loan', 'loud', 'lane', 'lapel', 'laugh', 'lava', 'light', 'local', 'loopy', 'lousy', 'lure', 'late', 'league', 'latch',
+    'lentil', 'lemon', 'lethal', 'lesson', 'lift', 'limits', 'liquor', 'listen', 'litter', 'lizard', 'locker', 'locate', 'lodge', 'locket', 'loofah', 'lock', 'locked',
+    'log', 'logger', 'look', 'lookout', 'lotion', 'lurk', 'lychee', 'lanyard', 'lawn', 'lawnmower', 'lasagne', 'lawsuit', 'leotard', 'lettuce', 'limb', 'limp', 'lei',
+    'lollipop', 'loyal', 'loyalty', 'lug', 'ladle', 'lady', 'lake'
   ],
   m: [
     'monster', 'medicine', 'melt', 'march', 'mark', 'mean', 'major', 'mild', 'minty', 'money', 'moving', 'motivate', 'myth', 'magical', 'mango', 'mayhem', 'merge',
-    'method', 'middle', 'mildly', 'mostly', 'mock', 'machine', 'manatee', 'mammoth', 'map', 'marsh', 'mayor', 'menace', 'merchandise', 'microscope'
+    'method', 'middle', 'mildly', 'mostly', 'mock', 'machine', 'manatee', 'mammoth', 'map', 'marsh', 'mayor', 'menace', 'merchandise', 'microscope', 'marker', 'malt',
+    'mask', 'masked', 'menu', 'macaw', 'magma', 'manga', 'mania', 'medal', 'metal', 'metallic', 'mesh', 'mile', 'mince', 'minus', 'mitt', 'mixed', 'misty', 'monk',
+    'monkey', 'moose', 'moo', 'mulch', 'muggy', 'mug', 'munch', 'mushy', 'mutt', 'magician', 'maid', 'maiden', 'malice', 'mall', 'mallet', 'mallard', 'melting', 'mantis',
+    'manual', 'marlin', 'market', 'maroon', 'marrow', 'martyr', 'mosque', 'majestic', 'majesty', 'matter', 'martini', 'meager', 'meant', 'mental', 'median', 'medic',
+    'medical', 'menial', 'menagerie', 'merit', 'merged', 'meteor', 'meter', 'mingle', 'miffed', 'malady', 'miner', 'minor', 'mirage', 'modem', 'modern', 'mohawk',
+    'mogul', 'mojito', 'moment', 'morsel', 'morph', 'moss', 'mold', 'moldy', 'mildew', 'mucous', 'mechanic', 'mechanical', 'muddle', 'muddy', 'muffin', 'muffle',
+    'mugged', 'murmur', 'muster', 'mutate', 'mutant', 'mutiny', 'muzzle', 'mystic'
   ],
   n: [
-    'noun', 'nice', 'novice', 'near', 'nest', 'nerdy', 'nut', 'nation', 'nasal', 'nearby', 'nickel', 'nimble', 'nobody', 'notice', 'notion', 'numbers', 'nature',
-    'nectar', 'necklace', 'negate', 'noble', 'noisy', 'noodle', 'novelty', 'navigate', 'neutral', 'nominate', 'nonsense', 'nuclear', 'narrate'
+    'noun', 'nice', 'novice', 'near', 'nest', 'nerdy', 'nut', 'nation', 'nasal', 'nearby', 'nickel', 'nimble', 'nobody', 'notice', 'notion', 'number', 'nature',
+    'nectar', 'necklace', 'negate', 'noble', 'noisy', 'noodle', 'novelty', 'navigate', 'neutral', 'nominate', 'nonsense', 'nuclear', 'narrate', 'nap', 'nod', 'nub',
+    'news', 'neon', 'nil', 'naked', 'naan', 'nacho', 'nails', 'narc', 'nasty', 'neigh', 'nana', 'needy', 'navy', 'narrow', 'name', 'named', 'napkin', 'niece', 'newt',
+    'nomad', 'noise', 'nook', 'noon', 'normal', 'north', 'notes', 'nutty', 'nymph', 'native', 'neat', 'nausea', 'neatly', 'needle', 'negative', 'nestle', 'numeric',
+    'numb', 'nimbus', 'nobility', 'nosy', 'nuzzle', 'nutmeg', 'nebula', 'nemesis', 'nephew', 'nesting', 'nervous', 'network', 'net', 'nicotine', 'nifty', 'normalize',
+    'nightly', 'nocturne', 'nocturnal', 'noteworthy', 'notable', 'nougat', 'nuance', 'nucleus', 'nugget', 'nullify', 'null', 'nunchuks', 'nursery', 'narrator',
+    'national', 'neck', 'nautical', 'neighbor', 'neuter', 'neutralize', 'nibble', 'nickname', 'nostril', 'numerous', 'navigator', 'negotiate', 'newspaper', 'night',
+    'notebook', 'nationality', 'nectarine', 'nonchalant', 'nonfiction', 'nutrition'
   ],
   o: [
     'octopus', 'olive', 'oatmeal', 'orb', 'ocean', 'odor', 'optical', 'ought', 'owner', 'obtuse', 'oldest', 'onward', 'oracle', 'outage', 'otter', 'oxygen',
-    'obscure', 'offense', 'ominous', 'opaque', 'orchid', 'osmosis', 'outgrow', 'outlaw', 'outstanding', 'oversee', 'obstacle', 'orient'
+    'obscure', 'offense', 'ominous', 'opaque', 'orchid', 'osmosis', 'outgrow', 'outlaw', 'outstanding', 'oversee', 'obstacle', 'orient', 'oaf', 'one', 'old', 'out',
+    'own', 'owl', 'obey', 'oats', 'ogre', 'okra', 'once', 'omen', 'opal', 'opus', 'open', 'opening', 'owned', 'oasis', 'obese', 'obesity', 'orbit', 'occur', 'octagon',
+    'oil', 'oily', 'oiled', 'onion', 'opera', 'orange', 'orzo', 'ounce', 'oust', 'outro', 'ovary', 'ozone', 'oblong', 'obsess', 'oboe', 'obtain', 'oblige', 'occupy',
+    'occupant', 'object', 'office', 'offer', 'offering', 'onset', 'onward', 'oppose', 'opposition', 'opposite', 'ordain', 'organ', 'origin', 'original', 'other',
+    'output', 'outfit', 'observe', 'observant', 'observer', 'ocarina', 'obvious', 'offhand', 'omelet', 'opacity', 'optimal', 'optimum', 'orderly', 'order', 'orphan',
+    'origami', 'oriole', 'ostrich', 'outcome', 'outline', 'outskirts', 'overdue', 'over', 'overjoyed', 'overeat', 'overwhelm', 'overworked', 'obelisk', 'odorless'
   ],
   p: [
     'pencil', 'penguin', 'palindrome', 'pen', 'pun', 'pail', 'pelt', 'paint', 'panic', 'perch', 'pinch', 'pillar', 'plaza', 'proud', 'promotion', 'panel', 'paddle',
-    'papers', 'parking', 'parsley', 'pauper', 'peanut', 'peril', 'phlegm', 'pickled', 'pixel', 'piston', 'peace', 'pollen', 'proven'
+    'papers', 'parking', 'parsley', 'pauper', 'peanut', 'peril', 'phlegm', 'pickled', 'pixel', 'piston', 'peace', 'pollen', 'proven', 'pure', 'pact', 'palm', 'psalm',
+    'paid', 'payment', 'papa', 'part', 'path', 'pass', 'pear', 'peck', 'peek', 'phew', 'pier', 'plan', 'pile', 'pink', 'pine', 'play', 'plum', 'plot', 'poke', 'polo',
+    'poet', 'pond', 'pong', 'ping', 'pope', 'pork', 'pore', 'posh', 'prop', 'pump', 'punt', 'pup', 'puppy', 'past', 'pilot', 'pinecone', 'playful', 'poetry', 'poem',
+    'porcupine', 'porous', 'propel', 'propeller', 'pumped', 'pastime', 'pack', 'packing', 'pasta', 'parka', 'parry', 'pastry', 'partial', 'paver', 'peer', 'petite',
+    'pristine', 'peerless', 'pesto', 'phase', 'phishing', 'photo', 'photography', 'photographer', 'pique', 'pliers', 'pitch', 'pivot', 'pixie', 'pitcher', 'pooch',
+    'poppy', 'pout', 'prank', 'prose', 'poultry', 'power', 'powerful', 'praise', 'pride', 'print', 'prison', 'prisoner', 'puffy', 'purse', 'putt', 'putty', 'peruse'
   ],
   q: [
     'queen', 'quilt', 'quiver', 'quiz', 'quart', 'quack', 'quads', 'qualm', 'quake', 'quell', 'quill', 'query', 'quirk', 'quote', 'quota', 'quiet', 'quitting',
     'quail', 'quarry', 'quadratic', 'qualify', 'quality', 'quicken', 'quickly', 'quantify', 'quest', 'quotient', 'quadruple'
   ],
   r: [
-    'rooster', 'rat', 'realm', 'rain', 'real', 'rental', 'rebel', 'reins', 'reuse', 'rhyme', 'resemble', 'rocks', 'rotate', 'relevant', 'racing', 'radish',
-    'radiation', 'raid', 'random', 'rattle', 'rebate', 'recipe', 'reheat', 'rectify', 'repair', 'recommend', 'resize', 'rinse', 'rookie', 'ruined'
+    'rooster', 'rat', 'realm', 'rain', 'real', 'renter', 'rebel', 'reins', 'reuse', 'rhyme', 'resemble', 'rocks', 'rotate', 'relevant', 'racing', 'radish',
+    'radiation', 'raid', 'random', 'rattle', 'rebate', 'recipe', 'reheat', 'rectify', 'repair', 'recommend', 'resize', 'rinse', 'rookie', 'ruined', 'rig', 'red',
+    'ram', 'raw', 'rib', 'rim', 'rip', 'rod', 'rob', 'rub', 'row', 'rot', 'run', 'rum', 'race', 'rack', 'raft', 'rafter', 'rage', 'rail', 'ramp', 'rant', 'rank',
+    'rare', 'rash', 'read', 'reap', 'rear', 'rent', 'rice', 'rich', 'ride', 'rind', 'riot', 'ripe', 'rise', 'risk', 'roar', 'robe', 'roll', 'role', 'roof', 'rook',
+    'room', 'rope', 'rose', 'rosy', 'route', 'ruby', 'rude', 'ruin', 'rug', 'runt', 'rush', 'rust', 'raccoon', 'ready', 'ripped', 'robber', 'robbed', 'rubber',
+    'rowing', 'rotten', 'runner', 'running', 'racecar', 'racetrack', 'rake', 'railroad', 'ranking', 'rarity', 'reading', 'reaper', 'riches', 'riding', 'risky',
+    'rolling', 'roomy', 'routing', 'rushing', 'radiate', 'radio', 'rancid', 'rapier', 'rarely', 'rather', 'rating', 'ravine', 'reason', 'reassured', 'reasonable',
+    'reboot', 'rebuke', 'recite', 'record', 'recede', 'recess', 'redeem', 'reflect', 'refine', 'refund', 'reign', 'relive', 'relish', 'relic', 'reload', 'remind',
+    'remark', 'remote', 'repay', 'repeat', 'report', 'resent', 'resin', 'retell', 'retort', 'reverse', 'revert', 'revise', 'revive', 'rhythm', 'riddle', 'rifle',
+    'rigorous', 'right', 'rivet', 'roam', 'rival', 'rising', 'rocket', 'rodent', 'root', 'round', 'rubble', 'ruler', 'rumble', 'rumor', 'rounded', 'racist'
   ],
   s: [
     'snake', 'savannah', 'spork', 'spare', 'saucy', 'serious', 'saga', 'saute', 'sense', 'shaky', 'salty', 'shore', 'slang', 'snarl', 'sonic', 'safari', 'salmon',
-    'saying', 'scorch', 'scald', 'seasoned', 'shaped', 'shoes', 'silent', 'slimy', 'smiling', 'sailing', 'solemn', 'sandbox', 'scarlet'
+    'saying', 'scorch', 'scald', 'seasoned', 'shaped', 'shoes', 'silent', 'slimy', 'smiling', 'sailing', 'solemn', 'sandbox', 'scarlet', 'sad', 'sap', 'saw', 'sea',
+    'say', 'sew', 'shy', 'sin', 'sip', 'ski', 'son', 'sob', 'spy', 'saw', 'soy', 'sub', 'sun', 'sum', 'sack', 'safe', 'sage', 'said', 'sail', 'sale', 'salt', 'same',
+    'sand', 'sane', 'sank', 'sash', 'sang', 'scar', 'scum', 'seal', 'seam', 'sear', 'seel', 'seed', 'seek', 'self', 'sell', 'send', 'serf', 'shop', 'shot', 'show',
+    'shun', 'sick', 'side', 'sigh', 'sign', 'silk', 'silo', 'silt', 'sing', 'sink', 'skew', 'skid', 'skip', 'skin', 'slap', 'slab', 'slaw', 'slay', 'slid', 'sled',
+    'slum', 'slow', 'slur', 'smog', 'snag', 'snap', 'snip', 'snot', 'snow', 'snug', 'soap', 'soak', 'soar', 'soda', 'sock', 'soft', 'soil', 'sofa', 'sole', 'solo',
+    'some', 'song', 'soon', 'spam', 'span', 'spar', 'spit', 'spin', 'star', 'stab', 'stay', 'stem', 'stew', 'stir', 'stop', 'stub', 'stow', 'stun', 'sung', 'sunk',
+    'surf', 'sure', 'swab', 'swan', 'swat', 'sway', 'sync', 'shade', 'salad', 'sauna', 'stark', 'savor', 'savvy', 'scene', 'scoop', 'scold', 'scoot', 'scorn', 'scout',
+    'scrub', 'seize', 'serve', 'sever', 'sewer', 'shack', 'shaft', 'shake', 'shall', 'shard', 'shape', 'shark', 'shave', 'shawl', 'sheep', 'sheer', 'sheet', 'shine',
+    'shoal', 'shock', 'shook', 'shoot', 'short', 'shove', 'shorts', 'shrewd', 'shush', 'siege', 'sieve', 'sight', 'sinus', 'skate', 'skirt', 'slash', 'slate', 'slack',
+    'sleep', 'slide', 'slime', 'sling', 'slush', 'slurp', 'smart', 'smash', 'smear', 'smell', 'sneer', 'sniff', 'snipe', 'snoop', 'snort', 'solid', 'sound', 'space',
+    'speck', 'spell', 'spent', 'spend', 'spice', 'spicy', 'spill', 'split', 'spook', 'spool', 'spoon', 'sport', 'spoil', 'squid', 'staff', 'stain', 'stair', 'stale',
+    'stall', 'stalk', 'stare', 'stash', 'steer', 'steep', 'steel', 'stitch', 'stick', 'stock', 'stoic', 'stomp', 'stood', 'straw', 'stunt', 'style', 'super', 'sweep',
+    'swing', 'swirl'
   ],
   t: [
     'tulip', 'tunic', 'tasty', 'tail', 'tear', 'tasks', 'taunt', 'thank', 'tiger', 'topic', 'traded', 'talking', 'tenant', 'tense', 'things', 'thorny', 'thrifty',
